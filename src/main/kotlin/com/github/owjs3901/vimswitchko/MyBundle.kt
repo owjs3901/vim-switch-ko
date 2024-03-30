18c903e682ee67e9aadfc90a1740346b0808fe83
GitHub Action <action@github.com>
2024-03-30 10:08:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/owjs3901/vimswitchko/MyBundle.kt
+++ src/main/kotlin/com/github/owjs3901/vimswitchko/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.owjs3901.vimswitchko
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/owjs3901/vimswitchko/services/MyProjectService.kt
+++ src/main/kotlin/com/github/owjs3901/vimswitchko/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.owjs3901.vimswitchko.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.owjs3901.vimswitchko.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
