e677a4e594c11921e5159087731cac99c895fb2b
owjs3901 <owjs3901@gmail.com>
2024-04-20 16:58:01 +0900

    Only Apply Blocking IM in the Editor component Update Change IM when the Editor component is focused with VIM Normal Mode

--- src/main/kotlin/com/devfive/vim_switch_ko/VimSwitchKoListener.kt
+++ src/main/kotlin/com/devfive/vim_switch_ko/VimSwitchKoListener.kt
@@ -1,18 +1,18 @@
 package com.devfive.vim_switch_ko
 
-import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.IdeEventQueue
 import com.intellij.ide.plugins.PluginManager
 import com.intellij.ide.plugins.cl.PluginAwareClassLoader
+import com.intellij.openapi.editor.impl.EditorComponentImpl
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.openapi.startup.StartupActivity
 import java.awt.event.KeyEvent
 import java.awt.im.InputContext
+import javax.swing.FocusManager
 
 internal class VimSwitchKoListener : ProjectActivity, DumbAware {
     override suspend fun execute(project: Project) {
@@ -26,31 +26,33 @@ internal class VimSwitchKoListener : ProjectActivity, DumbAware {
             return enabled
         }
 
-        fun toEnglishIME(event: KeyEvent) {
-            val context = (event.source.javaClass.getMethod("getInputContext").invoke(event.source) as InputContext)
-            context.setCharacterSubsets(null)
+        fun toEnglishIME(context: InputContext?) {
+            context?.setCharacterSubsets(null)
             InputContext.getInstance().setCharacterSubsets(null)
         }
 
-        var editors:List<Any>?  = null
-        fun loadEditors(){
+        fun isFocusedEditor(): Boolean {
+            return FocusManager.getCurrentManager().focusOwner is EditorComponentImpl
+        }
+
+        var editors: List<Any>? = null
+        fun loadEditors() {
             val pluginDescriptor = PluginManager.getLoadedPlugins().find { it.pluginId == PluginId.getId("IdeaVIM") }
-            if(pluginDescriptor == null)
+            if (pluginDescriptor == null)
                 return
             val pluginClassLoader = pluginDescriptor.pluginClassLoader as PluginAwareClassLoader
             val injectClass = pluginClassLoader.tryLoadingClass("com.maddyhome.idea.vim.api.VimInjectorKt", true)
             val instance = injectClass?.getMethod("getInjector")!!.invoke(null)
             val editorGroup = instance.javaClass.getMethod("getEditorGroup").invoke(instance)
             editors = editorGroup.javaClass.getMethod("getEditors").invoke(editorGroup) as ArrayList<Any>
-
         }
 
 
-        fun isCurrentModeNormal(): Boolean {
+        fun isCurrentModeNormal(forcedLoad: Boolean = false): Boolean {
             ProjectManager.getInstance().openProjects.forEach { project ->
-                if(editors == null){
+                if (editors == null || forcedLoad) {
                     loadEditors()
-                    if(editors == null)
+                    if (editors == null)
                         return false
                 }
                 val currentFile = FileEditorManager.getInstance(project).selectedEditor?.file?.path ?: return false
@@ -64,27 +66,33 @@ internal class VimSwitchKoListener : ProjectActivity, DumbAware {
             }
             return false
         }
+        FocusManager.getCurrentManager().addPropertyChangeListener("focusOwner") {
+            if (isFocusedEditor() && enableIdeaVIM() && isCurrentModeNormal(true))
+                toEnglishIME((it.newValue as EditorComponentImpl).inputContext)
+        }
+
         IdeEventQueue.getInstance().addDispatcher(IdeEventQueue.EventDispatcher { e ->
             if (e !is KeyEvent) return@EventDispatcher false
-            if (!enableIdeaVIM()) {
+            if (!isFocusedEditor())
                 return@EventDispatcher false
-            }
+
+            if (!enableIdeaVIM())
+                return@EventDispatcher false
             if (e.id != KeyEvent.KEY_PRESSED) {
                 if (e.keyCode == 0 && e.keyChar.code == 65535 && isCurrentModeNormal()) {
                     // Switching to English IME when switching korean mode in normal mode
-                    toEnglishIME(e)
+                    toEnglishIME(e.component.inputContext)
                 }
                 return@EventDispatcher false
             }
 
             if (e.keyCode == KeyEvent.VK_ESCAPE) {
                 // Always switch to English IME when pressing ESC
-                toEnglishIME(e)
+                toEnglishIME(e.component.inputContext)
             } else if (e.isControlDown && e.keyCode == KeyEvent.VK_C) {
                 // Switch to English IME when pressing Ctrl+C in normal mode
-                if (!isCurrentModeNormal()) {
-                    toEnglishIME(e)
-                }
+                if (!isCurrentModeNormal())
+                    toEnglishIME(e.component.inputContext)
             }
             return@EventDispatcher false
         }, null)
